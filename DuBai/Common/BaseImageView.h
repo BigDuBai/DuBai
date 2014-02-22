bfc298401df6e25ef7eb1268c3925e70380e546a
tw365524764 <365524764@qq.com>
2014-02-22 23:38:50 +0800

    image+lightlable

---
+++ DuBai/Common/BaseImageView.h
@@ -0,0 +1,13 @@
+//
+//  BaseImageView.h
+//  DuBai
+//
+//  Created by darktm on 14-2-22.
+//  Copyright (c) 2014年 darktm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseImageView : UIImageView
+
+@end
---
+++ DuBai/Common/BaseImageView.m
@@ -0,0 +1,31 @@
+//
+//  BaseImageView.m
+//  DuBai
+//
+//  Created by darktm on 14-2-22.
+//  Copyright (c) 2014年 darktm. All rights reserved.
+//
+
+#import "BaseImageView.h"
+
+@implementation BaseImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
